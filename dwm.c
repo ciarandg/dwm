ddd468538a2388b46dac7110fc31dc7854196497
Ciaran <ciaran.deg@gmail.com>
2020-12-03 16:54:27 -0700

    fix gaps

--- dwm.c
+++ dwm.c
@@ -682,6 +682,10 @@ createmon(void)
 	m->nmaster = nmaster;
 	m->showbar = showbar;
 	m->topbar = topbar;
+	m->gappih = gappih;
+	m->gappiv = gappiv;
+	m->gappoh = gappoh;
+	m->gappov = gappov;
 	m->lt[0] = &layouts[0];
 	m->lt[1] = &layouts[1 % LENGTH(layouts)];
 	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
